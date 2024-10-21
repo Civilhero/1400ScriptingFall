47f47b2d9fbdf9f038a4fafe6d4598753cdf4153
Allen Larsen <allenlarsen444@gmail.com>
2024-10-21 13:51:51 -0600

    finished the sawblade

---
+++ My project/Assets/Scriptor/TrigEvent.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrigEvent : MonoBehaviour
+{
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter2D(Collider2D collision)
+    { if (collision.tag == "Player")
+        {
+            triggerEvent.Invoke();
+            Debug.Log("Player interacted with the object!");
+        }
+    }
+}
