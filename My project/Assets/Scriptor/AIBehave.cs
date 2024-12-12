3b2258cb0064a53e00f4bfb3371c10f24bfa3281
Allen Larsen <allenlarsen444@gmail.com>
2024-12-12 16:24:13 -0700

    the units are done, now the finale

--- My project/Assets/Scriptor/AIBehave.cs
+++ My project/Assets/Scriptor/AIBehave.cs
@@ -5,20 +5,26 @@ using UnityEngine.AI;
 
 public class AIBehave : MonoBehaviour
 {
-    public Transform target;
-    private NavMeshAgent agent;
-    
-    private void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;
-    }
+	public Transform[] points;
+	private int destPoint = 0;
+	private NavMeshAgent agent;
 
-    private void Update()
+    void Start()
     {
-        if (target != null)
-        {
-            agent.SetDestination(target.position);
-        }
+		agent = GetComponent<NavMeshAgent>();
+		GotoNextPoint();
+		agent.updateRotation = false;
+    }
+    void GotoNextPoint()
+	{
+		if (points.Length == 0)
+			return;
+		agent.destination = points[destPoint].position;
+		destPoint = (destPoint + 1) % points.Length;
+	}
+    void Update()
+    {
+		if (!agent.pathPending && agent.remainingDistance < 0.5f)
+			GotoNextPoint();
     }
 }
